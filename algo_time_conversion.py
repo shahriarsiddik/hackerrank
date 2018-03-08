71418be0d0d106ec12a32b1fbf4c4f61f57e14af
shahriarsiddik <shahriarsiddik@gmail.com>
2018-03-08 10:26:31 +0600

    new problems

--- algo_time_conversion.py
+++ algo_time_conversion.py
@@ -1,9 +1,22 @@
-# 07:05:45PM
-time = "07:05:45PM"
 
-if time[8:]=="PM":
-    hr = int(time[:2])+12
-    print str(hr)+time[2:-2]
-else:
-    print time[:-2]
+
+
+import sys
+
+def timeConversion(s):
+    if s[8:] == "PM":
+        if s[:2]=='12':
+            return s[:-2]
+        else:
+            hr = int(s[:2]) + 12
+            return str(hr) + s[2:-2]
+    else:
+        if s[:2]=='12':
+            return '00'+s[2:-2]
+
+        return s[:-2]
+
+s = raw_input().strip()
+result = timeConversion(s)
+print(result)
 
--- python_lists.py
+++ python_lists.py
@@ -4,18 +4,34 @@ if __name__ == '__main__':
 res = []
 for i in range(N):
     inp = raw_input().split()
-    if inp[0] == 'insert':
-        res.insert(int(inp[1]),int(inp[2]))
-    elif inp[0] == 'print':
+    if inp[0] == 'print':
         print res
-    elif inp[0] == 'remove':
-        res.remove(int(inp[1]))
-    elif inp[0] == 'append':
-        res.append(int(inp[1]))
-    elif inp[0] == 'sort':
-        res.sort()
-    elif inp[0] == 'pop':
-        res.pop()
     else:
-        res.reverse()
+        if len(inp) == 1:
+            eval("res.{}()".format(inp[0]))
+
+        elif len(inp) == 2:
+            eval("res.{}({})".format(inp[0], inp[1]))
+        else:
+            eval("res.{}({},{})".format(inp[0], inp[1], inp[2]))
+
+
+
+        # eval("res.{}".format(inp[0])) if len(inp)==1 else eval("res.{}({},{})".format(inp[0],inp[1],inp[2])) if len(inp)==3 else eval("res.{}({})".format(inp[0],inp[1]))
+
+
+    # if inp[0] == 'insert':
+    #     res.insert(int(inp[1]),int(inp[2]))
+    # elif inp[0] == 'print':
+    #     print res
+    # elif inp[0] == 'remove':
+    #     res.remove(int(inp[1]))
+    # elif inp[0] == 'append':
+    #     res.append(int(inp[1]))
+    # elif inp[0] == 'sort':
+    #     res.sort()
+    # elif inp[0] == 'pop':
+    #     res.pop()
+    # else:
+    #     res.reverse()
 #     print([res.insert(int(inp[1]),int(inp[2])) if inp[0] == 'insert' else res if inp[0] == 'print' else res.append(int(inp[1])) if inp[0] == 'append' else res.sort() if inp[0] == 'sort' else res.reverse()])
